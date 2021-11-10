c2c089e7f548e087cd5388927b0ecf0a923840aa
AdrianRodriguez20 <adrirodriguezfuentes@gmail.com>
2021-11-10 18:31:15 +0000

    Clinica

---
+++ Clinica/Modelo/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        private String user;
+        private String password;
+        public enum Rol
+        {
+            DIRECCION,
+            ADMINISTRATIVO,
+            ADMINISTRADOR,
+            PERSONAL_SANITARIO
+        }
+
+        public Usuario(string user, string password)
+        {
+            this.user = user;
+            this.password = password;
+        }
+    }
+}

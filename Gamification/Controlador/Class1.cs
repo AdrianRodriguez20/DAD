058bf3d0dcfb590bdedc5526ad6be84378459268
AdrianRodriguez20 <59927052+AdrianRodriguez20@users.noreply.github.com>
2021-12-02 00:20:44 +0000

    Gamificacion

---
+++ Gamification/Controlador/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo.db;
+using Modelo;
+
+namespace Controlador
+{
+    public class Class1
+    {
+       
+ 
+    }
+}
